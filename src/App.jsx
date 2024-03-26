88bbf2ef57de1f2262ffdf7007fd1bac72cdb605
Leon --Remote <leon@entwurf.co.uk>
2024-03-26 10:49:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+import DisplaySection from './components/DisplaySection';
+import Nav from './components/Nav';
+import SoundSection from './components/SoundSection';
+import Jumbotron from './components/jumbotron';
+import WebgiViewer from './components/WebgiViewer';
+
+function App() {
+	const webgiViewerRef = useRef();
+	const contentRef = useRef();
+
+	const handlePreview = () => {
+		webgiViewerRef.current.triggerPreview();
+	};
+
+	return (
+		<div className='App'>
+			<div ref={contentRef} id='content'>
+				<Nav />
+				<Jumbotron />
+				<SoundSection />
+				<DisplaySection triggerPreview={handlePreview} />
+			</div>
+
+			<WebgiViewer ref={webgiViewerRef} contentRef={contentRef} />
+		</div>
+	);
+}
+
+export default App;
